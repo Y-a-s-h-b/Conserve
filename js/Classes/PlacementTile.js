3c4d4963ab56db955fabcc6e243ebdf103d67165
Yash Bansal <89540665+Y-a-s-h-b@users.noreply.github.com>
2023-02-01 16:51:17 +0530

    Added New Sprites

--- js/Classes/PlacementTile.js
+++ js/Classes/PlacementTile.js
@@ -1,25 +1,27 @@
-class PlacementTile{
-    constructor({position = {x : 0, y : 0}}){
-        this.position = position
-        this.size = 64
-        this.color = 'rgba(255,255,255,0.1)'
-        this.occupied = false;
+class PlacementTile {
+    constructor({ position = { x: 0, y: 0 } }) {
+      this.position = position
+      this.size = 64
+      this.color = 'rgba(255, 255, 255, 0.15)'
+      this.occupied = false
     }
-    draw(){
-        c.fillStyle = this.color
-        c.fillRect(this.position.x,this.position.y,this.size,this.size)
+  
+    draw() {
+      c.fillStyle = this.color
+      c.fillRect(this.position.x, this.position.y, this.size, this.size)
     }
-    update(mouse){
-        this.draw()
-        if( mouse.x > this.position.x && 
-            mouse.x < this.position.x + this.size &&
-            mouse.y > this.position.y && 
-            mouse.y < this.position.y + this.size)
-        {
-            this.color = 'white'
-        }
-        else{
-            this.color = 'rgba(255,255,255,0.1)'
-        }
+  
+    update(mouse) {
+      this.draw()
+  
+      if (
+        mouse.x > this.position.x &&
+        mouse.x < this.position.x + this.size &&
+        mouse.y > this.position.y &&
+        mouse.y < this.position.y + this.size
+      ) {
+        this.color = 'white'
+      } else this.color = 'rgba(255, 255, 255, 0.15)'
     }
-}
+  }
+  
--- js/Classes/Sprite.js
+++ js/Classes/Sprite.js
@@ -1,56 +1,54 @@
-class Sprite{
+class Sprite {
     constructor({
-        position = {x:0, y:0},
-        imageSrc,
-        frames ={max: 1},
-        offset = {
-            x:0,
-            y:0
-        }
-    }){
-        this.position = position
-        this.image = new Image()
-        this.image.src = imageSrc
-        this.frames = {
-            max: frames.max,
-            current:0,
-            elapsed: 0,
-            hold: 6
-        }
-        this.offset = offset
+      position = { x: 0, y: 0 },
+      imageSrc,
+      frames = { max: 1,hold: 1 },
+      offset = { x: 0, y: 0 }
+    }) {
+      this.position = position
+      this.image = new Image()
+      this.image.src = imageSrc
+      this.frames = {
+        max: frames.max,
+        current: 0,
+        elapsed: 0,
+        hold: 6
+      }
+      this.offset = offset
     }
-    draw(){
-        const cropWidth = this.image.width/this.frames.max
-        const crop = {
-            position: {
-                x: cropWidth * this.frames.current,
-                y: 0
-            },
-            width: cropWidth,
-            height: this.image.height
-        }
-        c.drawImage(
-            this.image,
-            crop.position.x,
-            crop.position.y,
-            crop.width,
-            crop.height,
-            this.position.x + this.offset.x,
-            this.position.y + this.offset.y,
-            crop.width,
-            crop.height
-            )
-        }
-
-    update(){
-            //Animation
-        this.frames.elapsed++
-        console.log(this.frames.hold)
-        if(this.frames.elapsed % this.frames.hold ===0){
-            this.frames.current++
-            if(this.frames.current >= this.frames.max){
-                this.frames.current = 0
-            }
-        }
+  
+    draw() {
+      const cropWidth = this.image.width / this.frames.max
+      const crop = {
+        position: {
+          x: cropWidth * this.frames.current,
+          y: 0
+        },
+        width: cropWidth,
+        height: this.image.height
+      }
+      c.drawImage(
+        this.image,
+        crop.position.x,
+        crop.position.y,
+        crop.width,
+        crop.height,
+        this.position.x + this.offset.x,
+        this.position.y + this.offset.y,
+        crop.width,
+        crop.height
+      )
     }
-}
+  
+    update() {
+      // responsible for animation
+      this.frames.elapsed++
+      if (this.frames.elapsed % this.frames.hold === 0) {
+        this.frames.current++
+        if (this.frames.current >= this.frames.max) {
+          this.frames.current = 0
+        }
+      }
+    }
+  }
+  
